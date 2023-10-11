877ec0c5ce50792a6f94c5de4b08bd52d14d9810
Beefy-py <hoftkenny@gmail.com>
2023-10-11 16:37:28 -0300

    run indexjs before deployment
    output file name rename

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.js", // Entry point for your JavaScript
   output: {
-    filename: "bundle.js", // Output file name
+    filename: "index.js", // Output file name
     path: path.resolve(__dirname, "dist"), // Output directory
   },
 };
